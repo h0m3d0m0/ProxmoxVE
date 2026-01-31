939bd9ec1c772b03b7ead8f79ca0f70326c840bd
h0m3d0m0 <109484139+h0m3d0m0@users.noreply.github.com>
2026-02-01 00:16:21 +0100

    Update zigbee2mqtt.sh

--- ct/zigbee2mqtt.sh
+++ ct/zigbee2mqtt.sh
@@ -8,10 +8,10 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Zigbee2MQTT"
 var_tags="${var_tags:-smarthome;zigbee;mqtt}"
 var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-5}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
-var_version="${var_version:-13}"
+var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-0}"
 
 header_info "$APP"
